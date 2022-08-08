1488d0659b0c0bc3fe1e2ed63b61c7dffdb5da88
Federico Secco <federico.secco7@gmail.com>
2022-08-09 00:15:32 +0200

    added pets and shop pets

--- main.js
+++ main.js
@@ -1,5 +1,109 @@
-async function loadNames() {
+async function generateAssets() {
+    /* GET DATA FROM JSON FILE */
     const response = await fetch('test-api.json');
     const data = await response.json();
+    console.log(data);
+
+    /* SET COINS, HEALTH, TROPHIES, ROUND */
+    document.getElementById("coins").innerHTML=data.coins;
+    document.getElementById("health").innerHTML=data.health;
+    document.getElementById("trophies").innerHTML=data.trophies;
+    document.getElementById("round").innerHTML=data.round;
+
+    /* GENERATE ANIMALS:
+    <div class="asset" id="pet-x">
+        <img class="flex-item bordered-asset" src="assets/pet-bat.svg" alt="" id="pet-img-x">
+        <span class="pet-attack-value" id="attack-pet-X">X</span>
+        <span class="pet-health-value" id="health-pet-X">X</span>
+    </div>
+    */
+    data.pets.map((pet, index) => {
+        if(pet != null){
+            let playedAnimal = document.createElement("div");
+            playedAnimal.setAttribute("class", "asset");
+            playedAnimal.setAttribute("id", "pet-"+index);
+
+            let animalImg = document.createElement("img");
+            animalImg.setAttribute("class", "flex-item bordered-asset");
+            animalImg.setAttribute("src", "assets/"+pet.type+".svg");
+            animalImg.setAttribute("alt", pet.type);
+            animalImg.setAttribute("id", "pet-img-"+index);
+
+            let animalAttack = document.createElement("span");
+            animalAttack.setAttribute("class", "pet-attack-value");
+            animalAttack.setAttribute("id", "attack-pet-"+index);
+            animalAttack.innerHTML=pet.attack+pet.temporaryAttack;
+
+            let animalHealth = document.createElement("span");
+            animalHealth.setAttribute("class", "pet-health-value");
+            animalHealth.setAttribute("id", "health-pet-"+index);
+            animalHealth.innerHTML=pet.health+pet.temporaryHealth;
+
+            playedAnimal.appendChild(animalImg);
+            playedAnimal.appendChild(animalAttack);
+            playedAnimal.appendChild(animalHealth);
+
+            document.getElementById("pets").appendChild(playedAnimal);
+        }
+        else{
+            let playedAnimal = document.createElement("div");
+            playedAnimal.setAttribute("class", "asset");
+            playedAnimal.setAttribute("id", "pet-"+index);
+
+            let animalImg = document.createElement("img");
+            animalImg.setAttribute("class", "flex-item bordered-asset no-shadow");
+            animalImg.setAttribute("src", "assets/empty-asset.png");
+
+            playedAnimal.appendChild(animalImg);
+            document.getElementById("pets").appendChild(playedAnimal);
+        }
+
+    });
+
+    /* GENERATE SHOP ANIMALS */
+    data.shop.map((pet, index) => {
+        if(pet != null){
+            let playedAnimal = document.createElement("div");
+            playedAnimal.setAttribute("class", "asset");
+            playedAnimal.setAttribute("id", "pet-"+index);
+
+            let animalImg = document.createElement("img");
+            animalImg.setAttribute("class", "flex-item bordered-asset");
+            animalImg.setAttribute("src", "assets/"+pet.type+".svg");
+            animalImg.setAttribute("alt", pet.type);
+            animalImg.setAttribute("id", "pet-img-"+index);
+
+            let animalAttack = document.createElement("span");
+            animalAttack.setAttribute("class", "pet-attack-value");
+            animalAttack.setAttribute("id", "attack-pet-"+index);
+            animalAttack.innerHTML=pet.attack+pet.temporaryAttack;
+
+            let animalHealth = document.createElement("span");
+            animalHealth.setAttribute("class", "pet-health-value");
+            animalHealth.setAttribute("id", "health-pet-"+index);
+            animalHealth.innerHTML=pet.health+pet.temporaryHealth;
+
+            playedAnimal.appendChild(animalImg);
+            playedAnimal.appendChild(animalAttack);
+            playedAnimal.appendChild(animalHealth);
+
+            document.getElementById("shop-pets").appendChild(playedAnimal);
+        }
+        else{
+            let playedAnimal = document.createElement("div");
+            playedAnimal.setAttribute("class", "asset");
+            playedAnimal.setAttribute("id", "pet-"+index);
+
+            let animalImg = document.createElement("img");
+            animalImg.setAttribute("class", "flex-item bordered-asset no-shadow");
+            animalImg.setAttribute("src", "assets/empty-asset.png");
+
+            playedAnimal.appendChild(animalImg);
+            document.getElementById("shop-pets").appendChild(playedAnimal);
+        }
+
+    });
+
+    /* GENERATE FOOD */
 }
-loadNames();
+generateAssets();
